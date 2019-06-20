19807212bb7382de8a16be31eff0f9ce26306729
wuuhaokun <w.sirius@gmail.com>
2019-06-20 14:44:06 +0800

    alpha 0.1

---
+++ UUBuy/Model/CollectionModel.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionModel.swift
+//  UUBuy
+//
+//  Created by Jack on 2019/6/20.
+//  Copyright © 2019 com.timi.test.test. All rights reserved.
+//
+
+import Foundation
---
+++ UUBuy/ViewController/BuyListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BuyListViewController.swift
+//  UUBuy
+//
+//  Created by Jack on 2019/6/20.
+//  Copyright © 2019 com.timi.test.test. All rights reserved.
+//
+
+import UIKit
+
+class BuyListViewController: MeViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
